a3126430e760042fa8c1b93ea512f27d0639a9fc
Nadson Costa <146302133+nadson-costa@users.noreply.github.com>
2025-12-08 22:19:34 -0300

    feat: Aplicação de teste em Nest

---
+++ app/src/tracer.ts
@@ -0,0 +1,41 @@
+import { NodeSDK } from '@opentelemetry/sdk-node';
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+import {
+  MeterProvider,
+  PeriodicExportingMetricReader,
+} from '@opentelemetry/sdk-metrics';
+import { resourceFromAttributes } from '@opentelemetry/resources';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
+import {
+  ATTR_SERVICE_NAME,
+  ATTR_SERVICE_VERSION,
+} from '@opentelemetry/semantic-conventions';
+import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
+import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-grpc';
+
+const SERVICE_NAME = 'app-rocketseat';
+
+const metricsExporter = new OTLPMetricExporter({});
+const metricReader = new PeriodicExportingMetricReader({
+  exporter: metricsExporter,
+  exportIntervalMillis: 10000,
+});
+
+const traceExporter = new OTLPTraceExporter();
+
+const resource = resourceFromAttributes({
+  [ATTR_SERVICE_NAME]: 'app-rocketseat',
+  [ATTR_SERVICE_VERSION]: '1.0.0',
+});
+
+const mergedResource = resource;
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
+
+const sdk = new NodeSDK({
+  traceExporter,
+  instrumentations: [getNodeAutoInstrumentations()],
+  resource: mergedResource,
+  serviceName: SERVICE_NAME,
+});
+
+export { sdk };
